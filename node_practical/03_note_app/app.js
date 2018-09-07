54c5e3bbe5a0bad9ae00741268162853d88ded05
Shubham Suri <shubham013@live.com>
2018-09-07 11:59:52 +0530

    using npm and importing external libraries

--- node_practical/03_note_app/app.js
+++ node_practical/03_note_app/app.js
@@ -2,8 +2,14 @@ console.log("App.js Starting!!");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require("lodash");
 const notes = require('./notes.js');
+
 console.log(notes.addValue(5, -2));
+console.log(_.isString("Mayur"));
+console.log(_.isString(true));
+console.log(_.uniq(["Mayur",1,1,'mayur',2,5,7,'1']));
+
 
 // fs.appendFile( "TestFile.txt", `"HEllo  ${os.userInfo().username} !`, function(err) {
 //     if(err) {
