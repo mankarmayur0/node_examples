8d6c2aecfd65eda1afabef7649b5239b70802cc2
Shubham Suri <shubham013@live.com>
2018-09-07 12:47:56 +0530

    passing parameters to the node applications

--- node_practical/03_note_app/app.js
+++ node_practical/03_note_app/app.js
@@ -4,3 +4,20 @@ const fs = require('fs');
 const _ = require("lodash");
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+
+console.log(command);
+
+if( command === 'list' ) {
+    console.log("Display list of notes.");
+} else if( command === 'add' ) {
+    console.log("Adding a Note");    
+} else if( command === 'read' ) {
+    console.log("Reading a note");    
+} else if( command === 'remove' ){
+    console.log("Remove a note");    
+} else {
+    console.log("command not recognized.");    
+}
+
