665657a12b33ff2227abf49822c0b3f2accff09c
Shubham Suri <shubham013@live.com>
2018-09-18 18:18:59 +0530

    wiring up with weather.io

--- node_practical/04_weather_app/app.js
+++ node_practical/04_weather_app/app.js
@@ -1,8 +1,21 @@
-var geocode = require('./geocode/geocode');
+// var geocode = require('./geocode/geocode');
 
-geocode.getGeoAddress('getmapdata', (errorMessage, result) => {
-    if(errorMessage)
-        console.log(errorMessage);
-    else
-        console.log(JSON.stringify(result, undefined, 2));
-});
+// geocode.getGeoAddress('getmapdata', (errorMessage, result) => {
+//     if(errorMessage)
+//         console.log(errorMessage);
+//     else
+//         console.log(JSON.stringify(result, undefined, 2));
+// });
+
+
+// weather key : d46c33c7a7c0bacdd6db4ee760742584
+
+var request = require('request');
+
+request({
+            url: "https://api.darksky.net/forecast/d46c33c7a7c0bacdd6db4ee760742584/37.8267,-122.4233",
+            json: true
+        }, 
+        (error, request, body) => {
+            console.log(`Temperature: ${body.currently.temperature}`);
+        });
