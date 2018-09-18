e17845cc0f1cd31aee20e48934c773a1c2e9bb2f
Shubham Suri <shubham013@live.com>
2018-09-18 16:55:50 +0530

    Callback Errors

--- node_practical/04_weather_app/app.js
+++ node_practical/04_weather_app/app.js
@@ -4,7 +4,11 @@ request({
     url: 'https://ce954029-8387-43e8-95d0-a5a43ba6a65f.mock.pstmn.io/getmapdata',
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if( error ){
+        console.log("Uable to connect to the server.");
+    } else if(body.status === "OK"){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
