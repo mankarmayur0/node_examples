9f3087b14bf5811c78f3f233b7215ac4a0b5facc
Mayur Mankar <mayur.mankar@CALSOFTHQ.COM>
2018-09-28 09:57:38 +0530

    Introducing middlewares

--- node_practical/05_node_web_server/server.js
+++ node_practical/05_node_web_server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var hbs = require('hbs');
+var fs = require('fs');
 
 var app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentDate', () => {
     return new Date().getFullYear();
@@ -14,21 +16,38 @@ hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
-app.get('/', (req, resp) => {
-    resp.render('home.hbs', {
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `Time: ${now}, RequestType: ${req.method}, Path: ${req.path}`;
+    console.log(log);
+    fs.appendFile('system.log', log + '\n', ((err) => {
+        console.log('Error in requesting server to load.');
+    }));
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs', {
+        message: 'Site is under maintainance!',
+        pageName: 'Maintainance Page'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
         welcomeMessage: 'Welcome to Home Page!',
         pageName: "Home Page",
     })
 });
 
-app.get('/about', (req, resp) => {
-    resp.render('about.hbs', {
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
         pageName: "About Us Page",
     });
 });
 
-app.get('/bad', (req, resp) => {
-    resp.send({
+app.get('/bad', (req, res) => {
+    res.send({
         errorMessage: "Unable to server the request"
     });
 });
